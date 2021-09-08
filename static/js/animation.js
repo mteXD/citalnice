5c57855f9e7661cdede88cae57387d6dc423ba68
mteXD <matic.pozenel@gmail.com>
2021-09-08 20:35:19 +0200

    COmmit suicde

---
+++ static/js/animation.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+	document.getElementById("responsive-nav").style.height = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("responsive-nav").style.height = "0";
+}
